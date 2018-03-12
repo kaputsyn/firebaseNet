9f16881f1f0e97c32526843324f1ad08cf455209
OFFICEKIEV\a.koposov <a.koposov@temabit.com>
2018-03-12 17:35:47 +0200

    options

---
+++ src/FirebaseNet/Options/FirebaseOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirebaseNet.Options
+{
+    public class FirebaseOptions
+    {
+        public string ServiceKey { get; set; }
+        public string Proxy { get; set; }
+    }
+}
